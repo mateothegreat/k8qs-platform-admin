875d65e3bbc8310ab2da152cc0f22d086838aa53
Matthew Davis <matthew@matthewdavis.io>
2019-02-02 07:09:37 -0500

    removed k8 nodeSelector

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,3 +5,69 @@ export const environment = {
     API_BASE: 'https://api.k8qs.com'
 
 };
+
+
+$(document).ready(function () {
+
+    let guilds = { ...{
+    {
+        guilds;
+    }
+}
+}
+    ;
+    console.log(guilds);
+
+    $('#guildSelection').change(function () {
+
+        let value = $(this).val();
+        let html = '<option> </option>';
+
+        if (value) {
+
+            const select = $('#guildSelection');
+
+            select.empty();
+
+            guilds.forEach(function (guild) {
+
+                guild.channels.forEach(function (channel) {
+
+                    select.append('<option value="' + channel.id + '">' + channel.name + '</option>');
+
+                });
+
+            });
+
+        }
+
+    });
+
+});
+
+class A {
+    constructor() {
+        this.a();
+    }
+
+    a() { // returns nothing ---^
+
+        try {
+
+            this.b();
+
+            console.log(1); // this will NOT be called
+
+        } catch (e) {
+
+            /// do nothing
+
+        }
+
+    }
+
+    b() {
+        throw 'this does nothging';
+    }
+
+}
